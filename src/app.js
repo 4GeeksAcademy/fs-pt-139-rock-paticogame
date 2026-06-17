1f38429d8d8ddd4cafaa30f671229c715eebb488
Mariana <navarrosmariana46@gmail.com>
2026-06-17 11:08:25 +0000

    juegolisto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,32 @@
 //write your code here
 console.log("Hello Rigo from the console!");
 
+const juego = (userChoice) => {
+  const opcionUsuario = userChoice 
+  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+  const opcionComputadora =
+    choices[Math.floor(Math.random() * choices.length)];
+
+  const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"],
+  };
+
+  let resultado;
+
+if (opcionUsuario === opcionComputadora) 
+  {  resultado = "Draw";} 
+else if (rules[opcionUsuario].includes(opcionComputadora)) 
+  {  resultado = "You win";}
+else {  resultado = "You lose";}
+
+  console.log("Usuario eligio: " + opcionUsuario);
+  console.log("Computadora eligio: " + opcionComputadora);
+  console.log(resultado);
+};
+
+juego("scissors");
